e1459c0cf4fa34470f6a9c69c38f3edac1432775
hajuri07 <ibrahimhajuri07@gmail.com>
2025-08-31 17:08:39 +0530

    Update Web.py

--- Web.py
+++ Web.py
@@ -1,35 +1,29 @@
 import streamlit as st
 import pandas as pd
 
-# ------------------------
-# Page Selector
-# ------------------------
 st.sidebar.title("Pages")
 page = st.sidebar.selectbox("Choose Page", ["Dashboard", "SQL Queries"])
 
-# ------------------------
-# DASHBOARD PAGE
-# ------------------------
 if page == "Dashboard":
     st.title("🌎 Global Superstore Dashboard")
     
+    st.markdown("[Visit my GitHub](https://github.com/yourusername)")
+    
     st.subheader("Overview Charts")
     
-    # Display images
+  
     st.image("Sales Distribution by category.png", caption="Sales distribution by category", use_container_width= True)
     st.image("Top 10 products.png", caption="Top 10 Products by Revenue", use_container_width= True)
     st.image("monthly_sals.png", caption="Monthly Sales Trend", use_container_width= True)
     
     st.subheader("Cleaned Dataset Example")
-    # Load and show cleaned CSV (first 10 rows)
+   
     df = pd.read_csv("gs_clean.csv")
     st.dataframe(df.sample(10))
 
-# ------------------------
-# SQL QUERIES PAGE
-# ------------------------
 elif page == "SQL Queries":
     st.title("💻 SQL Queries")
+    st.markdown("[Visit my GitHub](https://github.com/yourusername)")
 
     queries = [
         {"title": "Cohort analysis", 
@@ -46,13 +40,14 @@ elif page == "SQL Queries":
          "csv_file": ""}
     ]
 
-    # Dropdown to select query
+    
     query_titles = [q["title"] for q in queries]
     selected_query = st.selectbox("Select Query", query_titles)
 
-    # Find the corresponding CSV file
+    
     csv_file = next(q["csv_file"] for q in queries if q["title"] == selected_query)
 
     st.subheader(selected_query)
     df_result = pd.read_csv(csv_file)
     st.dataframe(df_result)
+
